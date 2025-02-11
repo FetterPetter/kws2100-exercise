b3903574a9b38c2243c08da5e6d2e66efa257c7a
FetterPetter <petterskjorten@gmail.com>
2025-02-11 15:47:55 +0100

    added husky and typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import {createRoot} from "react-dom/client";
 import React from "react";
 import {Application} from "./modules/app/application";
-createRoot(document.getElementById("root")).render(<Application/>)
+createRoot(document.getElementById("root")!).render(<Application/>)
