0b14729eae270ed2e13945fdb2672237fcc00474
FetterPetter <petterskjorten@gmail.com>
2025-02-14 15:11:18 +0100

    added hover style and click for h1 municipality name

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,52 +1,85 @@
-import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import React, { useEffect, useRef, useState } from "react";
+import { Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
-// Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
-import { Stroke, Style } from "ol/style";
+import { Fill, Stroke, Style, Text } from "ol/style";
 
-// By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
-//  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
-// Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
-//  map data structure will be used
+const focusedStyle = () =>
+  new Style({
+    stroke: new Stroke({
+      width: 3,
+    }),
+    /*text: new Text(({
+    text: feature.getProperties().name,
+    fill: new Fill({color: "green"}),
+    stroke: new Stroke({color: "white", width:2})
+  })),*/
+    fill: new Fill({
+      color: "gold",
+    }),
+  });
+const unFocusedStyle = () =>
+  new Style({
+    stroke: new Stroke({
+      width: 5,
+      color: "crimson",
+    }),
+  });
 const osmLayer = new TileLayer({ source: new OSM() });
 const municipalityLayer = new VectorLayer({
   source: new VectorSource({
     url: "/kws2100-exercise/geojson/kommuner.geojson",
     format: new GeoJSON(),
   }),
-  style: new Style({
-    stroke: new Stroke({
-      color: "blue",
-      width: 2,
-    }),
-  }),
+  style: unFocusedStyle,
 });
 const map = new Map({
-  // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
-  view: new View({ center: [10.8, 59.9], zoom: 10 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
+  view: new View({ center: [10.8, 59.9], zoom: 7 }),
   layers: [osmLayer, municipalityLayer],
 });
 
-// A functional React component
 export function Application() {
-  // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
-  // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
-  // map React component
+  const [municipalityName, setMunicipalityName] = useState<string | null>(null);
+  function handlePointerMove(event: MapBrowserEvent<MouseEvent>) {
+    municipalityLayer
+      .getSource()
+      ?.forEachFeature((feature) => feature.setStyle(unFocusedStyle()));
+    municipalityLayer
+      .getSource()
+      ?.getFeaturesAtCoordinate(event.coordinate)
+      .forEach((feature) => {
+        feature.setStyle(focusedStyle);
+      });
+  }
+  function handleClick(event: MapBrowserEvent<MouseEvent>) {
+    municipalityLayer
+      .getSource()
+      ?.getFeaturesAtCoordinate(event.coordinate)
+      .forEach((feature) => {
+        setMunicipalityName(
+          feature.getProperties()["name"] ?? "her er det ikke noe",
+        );
+      });
+  }
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
+    map.on("pointermove", handlePointerMove);
+    map.on("click", handleClick);
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
-  return <div ref={mapRef}></div>;
+  return (
+    <>
+      <h1>{municipalityName}</h1>
+      <div ref={mapRef}></div>
+    </>
+  );
 }
