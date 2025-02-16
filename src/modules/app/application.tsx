005b17ebeb2f7adb0451d2e09a8bc75b640e3c13
FetterPetter <petterskjorten@gmail.com>
2025-02-16 13:08:50 +0100

    added schools

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -18,7 +18,7 @@ const focusedStyle = () =>
     stroke: new Stroke({
       width: 3,
     }),
-    /*text: new Text(({
+    /* text: new Text(({
     text: feature.getProperties().name,
     fill: new Fill({color: "green"}),
     stroke: new Stroke({color: "white", width:2})
@@ -42,15 +42,25 @@ const municipalityLayer = new VectorLayer({
   }),
   style: unFocusedStyle,
 });
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/kws2100-exercise/geojson/skoler.geojson",
+    format: new GeoJSON(),
+  }),
+});
 const map = new Map({
   view: new View({ center: [10.8, 59.9], zoom: 7 }),
-  layers: [osmLayer, municipalityLayer],
+  layers: [osmLayer, municipalityLayer, schoolLayer],
 });
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
   const [municipalityName, setMunicipalityName] = useState<string | null>(null);
   const [best, setBest] = useState(false);
+  const [currentKommunenummer, setCurrentKommunenummer] = useState<
+    string | null
+  >(null);
+  //schoolLayer.setVisible(false);
   let layerOn = true;
   function handlePointerMove(event: MapBrowserEvent<MouseEvent>) {
     if (layerOn) {
@@ -80,7 +90,9 @@ export function Application() {
       ?.getFeaturesAtCoordinate(event.coordinate)
       .forEach((feature) => {
         const name = feature.getProperties()["name"] ?? "her er det ikke noe";
+        const kommunenummer = feature.getProperties()["kommunenummer"];
         setMunicipalityName(name);
+        setCurrentKommunenummer(kommunenummer);
         if (name === "Bærum") {
           setBest(true);
         } else {
@@ -88,6 +100,7 @@ export function Application() {
         }
       });
   }
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
     map.on("pointermove", handlePointerMove);
@@ -107,8 +120,9 @@ export function Application() {
           </h1>
         )}
         <button onClick={handleLayerClick}>Skru av/på kommuner</button>
+
+        <div ref={mapRef}></div>
       </div>
-      <div ref={mapRef}></div>
     </>
   );
 }
